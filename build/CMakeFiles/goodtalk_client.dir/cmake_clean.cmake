45ba54af85c7b5851f2323c8888e19f7649a03e8
태현 <taehyunny0312@gmail.com>
2026-02-12 15:31:38 +0900

    goodtalk
    굿톡 프로그램

---
+++ build/CMakeFiles/goodtalk_client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goodtalk_client.dir/cJSON.c.o"
+  "CMakeFiles/goodtalk_client.dir/cJSON.c.o.d"
+  "CMakeFiles/goodtalk_client.dir/client_project.c.o"
+  "CMakeFiles/goodtalk_client.dir/client_project.c.o.d"
+  "goodtalk_client"
+  "goodtalk_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/goodtalk_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/goodtalk_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goodtalk_server.dir/cJSON.c.o"
+  "CMakeFiles/goodtalk_server.dir/cJSON.c.o.d"
+  "CMakeFiles/goodtalk_server.dir/server_project.c.o"
+  "CMakeFiles/goodtalk_server.dir/server_project.c.o.d"
+  "goodtalk_server"
+  "goodtalk_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/goodtalk_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
